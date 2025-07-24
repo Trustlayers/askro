a8d8c8a23b8a5639c0081277534c3db7727b9b0f
Trustlayers <williamolsen1989@icloud.com>
2025-07-24 08:35:09 +0000

    Update uninstall.php with standardized header from template

--- uninstall.php
+++ uninstall.php
@@ -1,16 +1,28 @@
 <?php
 /**
- * Plugin Uninstall Handler
- *
- * This file is executed when the plugin is uninstalled (deleted) from WordPress.
- * It handles cleanup of plugin data, options, and database tables.
- *
- * @package YourPlugin
+ * Plugin Uninstall Handler - removes all plugin data when uninstalled
+ * 
+ * @package ASKRO
+ * @author William Lowe
  * @since 1.0.0
+ * @license GPL-3.0-or-later
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/gpl-3.0.html.
  */
 
-// Security check: Ensure this file is only called during plugin uninstall
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+// If uninstall not called from WordPress, then exit.
+if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit;
 }
 
