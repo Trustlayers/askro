6db3781966186a18a12e186bdbbb2a3fb39d0cb4
Trustlayers <williamolsen1989@icloud.com>
2025-07-24 08:34:23 +0000

    feat(header): standardize main plugin header

--- askro.php
+++ askro.php
@@ -1,26 +1,41 @@
 <?php
 /**
- * Plugin Name: Askro
- * Plugin URI: https://github.com/your-username/askro
+ * Plugin Name: ASKRO - Ask and Answer Plugin
+ * Plugin URI: https://github.com/williamalowe/askro-plugin
  * Description: A comprehensive WordPress plugin for managing Q&A functionality with advanced features for community engagement and knowledge sharing.
  * Version: 1.0.0
- * Requires at least: 5.0
- * Requires PHP: 7.4
- * Author: Your Name
- * Author URI: https://yourwebsite.com
+ * Author: William Lowe
+ * Author URI: https://github.com/williamalowe
+ * License: GPL v3 or later
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain: askro
  * Domain Path: /languages
- * License: GPL v2 or later
- * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Requires at least: 5.0
+ * Tested up to: 6.7
+ * Requires PHP: 7.4
  * Network: false
- *
- * @package Askro
- * @author  Your Name
- * @since   1.0.0
+ * 
+ * @package ASKRO
+ * @author William Lowe
+ * @since 1.0.0
+ * @license GPL-3.0-or-later
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/gpl-3.0.html.
  */
 
-// Prevent direct access to this file
-if (! defined('ABSPATH')) {
+// Prevent direct access
+if (!defined('ABSPATH')) {
     exit;
 }
 
